f6b9ce88d9be93deaffe50503c6a14253d1b55b0
Elliot-Wang <45387491+Elliot-Wang@users.noreply.github.com>
2021-07-13 23:06:57 +0800

    从穷举递归到动态规划

--- src/main/java/class18_dp/Code01_RobotWalk.java
+++ src/main/java/class18_dp/Code01_RobotWalk.java
@@ -1,11 +1,25 @@
-package class18;
+package class18_dp;
 
+/**
+ * 简单的入门动态规划题
+ * 可是感觉就很难
+ * 一个机器人在一维的有限点上面来回走，规定在指定步数上
+ * 从起点走到终点，问有几种可达到的路径
+ */
 public class Code01_RobotWalk {
 
+    /**
+     * @param N 可走的点数量
+     * @param start 出发点
+     * @param aim 目的地
+     * @param K 走的步数
+     * @return 达到目标的路径数
+     */
 	public static int ways1(int N, int start, int aim, int K) {
 		if (N < 2 || start < 1 || start > N || aim < 1 || aim > N || K < 1) {
 			return -1;
 		}
+		// 利用穷举式的递归
 		return process1(start, K, aim, N);
 	}
 
@@ -15,25 +29,43 @@ public class Code01_RobotWalk {
 	// 有哪些位置？1~N
 	// 返回：机器人从cur出发，走过rest步之后，最终停在aim的方法数，是多少？
 	public static int process1(int cur, int rest, int aim, int N) {
-		if (rest == 0) { // 如果已经不需要走了，走完了！
+	    // 步数用尽，是否达到终点
+		if (rest == 0) {
 			return cur == aim ? 1 : 0;
 		}
-		// (cur, rest)
-		if (cur == 1) { // 1 -> 2
+
+		// 在两个边界上，只能往回走
+		if (cur == 1) {
 			return process1(2, rest - 1, aim, N);
-		}
-		// (cur, rest)
-		if (cur == N) { // N-1 <- N
+		}else if (cur == N) {
 			return process1(N - 1, rest - 1, aim, N);
 		}
-		// (cur, rest)
+
+		// 往左走，和往右走的情况加和
 		return process1(cur - 1, rest - 1, aim, N) + process1(cur + 1, rest - 1, aim, N);
 	}
 
+    /**
+     * 怎么利用动态规划优化上面的暴力穷举
+     * 思路，保存一些走法的信息
+     * 以及有了状态缓存表之后，这些缓存表能带来多大的提升？
+     * 这个可能是进一步优化的点
+     *
+     * __自顶向下的动态规划__
+     * @param N 可走的点数量
+     * @param start 出发点
+     * @param aim 目的地
+     * @param K 走的步数
+     * @return 达到目标的路径数
+     */
 	public static int ways2(int N, int start, int aim, int K) {
 		if (N < 2 || start < 1 || start > N || aim < 1 || aim > N || K < 1) {
 			return -1;
 		}
+
+		// 缓存表，存储内容是可行的路径数量
+        // 索引是当前状态，状态有两个数字：当前位置(狭义状态）+剩余步数(理解为时间参数)
+        // 先初始化缓存表
 		int[][] dp = new int[N + 1][K + 1];
 		for (int i = 0; i <= N; i++) {
 			for (int j = 0; j <= K; j++) {
@@ -64,25 +96,42 @@ public class Code01_RobotWalk {
 		} else {
 			ans = process2(cur - 1, rest - 1, aim, N, dp) + process2(cur + 1, rest - 1, aim, N, dp);
 		}
+		// 计算完之后保存中间结果
 		dp[cur][rest] = ans;
 		return ans;
-
 	}
 
+    /**
+     * 这个是把方法2的两个代码合在一起了？!
+     * 思考base case，优化缩减dp缓存表
+     * 然后根据base case，和暴力递归中的状态转移关系
+     * 逐步填写dp缓存表
+     * 当dp缓存表添加到我们需要的那个格子上时
+     * 答案得出！
+     *
+     * @param N 可走的点数量
+     * @param start 出发点
+     * @param aim 目的地
+     * @param K 走的步数
+     * @return 达到目标的路径数
+     */
 	public static int ways3(int N, int start, int aim, int K) {
 		if (N < 2 || start < 1 || start > N || aim < 1 || aim > N || K < 1) {
 			return -1;
 		}
 		int[][] dp = new int[N + 1][K + 1];
-		dp[aim][0] = 1;
+		dp[start][0] = 1;
 		for (int rest = 1; rest <= K; rest++) {
+		    // 第一行的状态转移
 			dp[1][rest] = dp[2][rest - 1];
+			// 第二至N-1行的状态转移
 			for (int cur = 2; cur < N; cur++) {
 				dp[cur][rest] = dp[cur - 1][rest - 1] + dp[cur + 1][rest - 1];
 			}
+			// 第N行的状态转移
 			dp[N][rest] = dp[N - 1][rest - 1];
 		}
-		return dp[start][K];
+		return dp[aim][K];
 	}
 
 	public static void main(String[] args) {
--- src/main/java/class18_dp/Code02_CardsInLine.java
+++ src/main/java/class18_dp/Code02_CardsInLine.java
@@ -1,8 +1,25 @@
-package class18;
+package class18_dp;
 
+/**
+ * 一个整数数组
+ * player A和player B依次从左右两端取值
+ * 最后取值最大者获胜
+ * 假设两位player都是用最优策略
+ * 求获胜者的分数
+ *
+ * 首先要知道获胜策略，然后按照获胜策略取分计算
+ */
 public class Code02_CardsInLine {
 
 	// 根据规则，返回获胜者的分数
+
+    /**
+     * 原来需要自己和对手两个回合之下的递归函数
+     * 有些复杂，穷举的递归栈都是树状结构
+     * 把递归调用的树画出来就清楚许多了，里面蕴含着状态转移的公式
+     * @param arr 整数数组
+     * @return 胜者的分数
+     */
 	public static int win1(int[] arr) {
 		if (arr == null || arr.length == 0) {
 			return 0;
@@ -32,6 +49,11 @@ public class Code02_CardsInLine {
 		return Math.min(p1, p2);
 	}
 
+    /**
+     * 穷举加上完整缓存表版本
+     * @param arr
+     * @return
+     */
 	public static int win2(int[] arr) {
 		if (arr == null || arr.length == 0) {
 			return 0;
@@ -82,6 +104,14 @@ public class Code02_CardsInLine {
 		return ans;
 	}
 
+    /**
+     * 推导dp缓存表的规律
+     * 利用状态转移关系运算得出结果
+     *
+     * 这两个表很有意思
+     * @param arr
+     * @return
+     */
 	public static int win3(int[] arr) {
 		if (arr == null || arr.length == 0) {
 			return 0;
@@ -89,9 +119,12 @@ public class Code02_CardsInLine {
 		int N = arr.length;
 		int[][] fmap = new int[N][N];
 		int[][] gmap = new int[N][N];
+		// base case是对角线
 		for (int i = 0; i < N; i++) {
 			fmap[i][i] = arr[i];
+			// gmap的对角线都是0
 		}
+		// 不断往右上角运算，得出结果
 		for (int startCol = 1; startCol < N; startCol++) {
 			int L = 0;
 			int R = startCol;
@@ -105,12 +138,33 @@ public class Code02_CardsInLine {
 		return Math.max(fmap[0][N - 1], gmap[0][N - 1]);
 	}
 
+    /**
+     * 一开始只考虑了一个递归
+     * emmm
+     * @param arr
+     * @return
+     */
+	public static int greedy(int[] arr) {
+	    return gg(arr, 0, arr.length - 1, 0);
+    }
+
+    public static int gg(int[] arr, int left, int right, int score) {
+	    if(left == right) {
+	        return arr[left];
+        } else if(left == right - 1) {
+	        return 0;
+        }
+	    int sub_left = arr[left] - Math.max(arr[left+1], arr[right]);
+	    int sub_right = arr[right] - Math.max(arr[left], arr[right - 1]);
+	    return (sub_left > sub_right) ? gg(arr, left + 1, right, score + arr[left])
+            : gg(arr, left, right-1, score+arr[right]);
+    }
+
 	public static void main(String[] args) {
 		int[] arr = { 5, 7, 4, 5, 8, 1, 6, 0, 3, 4, 6, 1, 7 };
 		System.out.println(win1(arr));
 		System.out.println(win2(arr));
 		System.out.println(win3(arr));
-
 	}
 
 }
